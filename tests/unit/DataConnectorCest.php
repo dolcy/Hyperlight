c4ed3db82a5c8a6ce55a2dc34d945279dd73043d
Shaughn Dolcy <sdolcy@gmail.com>
2019-12-13 16:48:13 -0500

    Update tests.

--- tests/unit/DataConnectorCest.php
+++ tests/unit/DataConnectorCest.php
@@ -1,20 +1,21 @@
 <?php declare(strict_types=1);
 
-use Hyperlight\Config\Database;
+use Hyperlight\Config\DataConnector;
 
 class DataConnectorCest
 {
-    public function _before(UnitTester $I)
+    public function _before(UnitTester $I): void
     {
-        $database = new Database();
-
-        return $database->dataConnector();
+        //
     }
 
     // tests
     public function testForDataConnection(UnitTester $I): void
     {
         $I->wantTo('test database host connection');
-        $I->expect('to get connection');
+        $I->expect('to get persistent connection');
+        $db = new DataConnector();
+        $connection = $db->connect();
+        $dbal = $db->abstractor();
     }
 }
