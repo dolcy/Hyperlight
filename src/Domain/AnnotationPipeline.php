571c20b59fba0fbe96eced96c4037ac759e062b3
Shaughn Dolcy <shaughn@dolcy.com>
2019-12-17 10:18:12 -0500

    Add AnnotationPipeline class/intial test.

---
+++ src/Domain/AnnotationPipeline.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hyperlight\Domain;
+
+class AnnotationPipeline
+{
+    public function build(): void
+    {
+        echo "Annotation definitions here.";
+    }
+}
