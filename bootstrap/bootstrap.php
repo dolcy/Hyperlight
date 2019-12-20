68e8b0b794e0ceedf6aaf4e9ea3c87a5b6b4533e
Shaughn Dolcy <sdolcy@gmail.com>
2019-12-19 21:18:47 -0500

    Add logging; update comments.

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -7,22 +7,35 @@ use Hyperlight\Domain\User\User;
 use function Siler\Dotenv\init;
 use Siler\Route;
 
-// require autoload
+/**
+ * initial bootstrap
+ */
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// error handler
+/**
+ * error handler
+ */
 require_once __DIR__ . '/_error.php';
 
-// init dotenv via siler helper
+/**
+ *  debug log
+ */
+require_once __DIR__ . '/_debug.php';
+
+/**
+ * init dotenv
+ */
 init(__DIR__ . '/../');
 
-// initial root route
+/**
+ * initialize cycle orm
+ */
+require_once __DIR__ . '/_cycle.php';
+
+// temporary route test
 Route\get('/', function (): void {
     echo 'Fantastic. We are green on root.<br><br>';
 });
-
-// initiate cycle orm
-require_once __DIR__ . '/_cycle.php';
 
 // temporary orm test
 $user = new User();
